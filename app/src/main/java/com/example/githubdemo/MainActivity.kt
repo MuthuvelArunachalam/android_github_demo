dc845d542b04e57192005477a949ec357098bc2e
MuthuvelEkambaranathan <muthuvel.e@securekloud.com>
2024-01-31 12:49:30 +0530

    Versioning Code commit First time

--- app/src/main/java/com/example/githubdemo/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.githubdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Change 1
+        //Change comment in versioniung
+        Log.d("Versioning Changes in versioning branch","Tag...1")
+        //Versioning branch
     }
 }
